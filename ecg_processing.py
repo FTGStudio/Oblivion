097e2b8a6f9df8f17171ea6e2dcefb34a8bed24d
Aaron Crump <air.crump@gmail.com>
2017-05-07 20:42:16 -0400

    saved my virtualenv requirements, fiddling with the biosppy lib

--- ecg_processing.py
+++ ecg_processing.py
@@ -3,15 +3,16 @@ from matplotlib import pyplot as pl
 from biosppy import storage
 from biosppy.signals import ecg
 
-signal, mdata = storage.load_txt('nicks_heart_1.csv')
+signal, mdata = storage.load_txt('nicks_heart_short_1.csv')
 Fs = mdata['sampling_rate']
 N = len(signal)
 T = (N-1)/Fs
 ts = np.linspace(0, T, N, endpoint=False)
-out = ecg.ecg(signal=signal, sampling_rate=Fs, show=True)
+out = ecg.ecg(signal=signal, sampling_rate=Fs, show=False)
 
 
 pl.plot(ts, signal, lw=2)
 pl.grid()
-pl.show()
+#pl.show()
+print(out)
 
