e2c942d7c310c3a3a09aaa345475cc8770831cd9
Luke Ganzevoort <lukeganzevoort@gmail.com>
2017-05-22 15:39:23 -0400

    organized and commented

--- ecg_lora.py
+++ ecg_lora.py
@@ -2,7 +2,12 @@ import serial
 import time
 from bitstring import BitArray
 
+# Todo
+#   * Look for if it's already connected to the server
+#   * Clear buffer before reading
+
 class lora:
+
     def __init__(self):
         # Set up serial port with LoRa mote
         self.ser = serial.Serial(
@@ -16,9 +21,11 @@ class lora:
         # Initialize the message id
         self.msgID = 0
 
+
     def send(self, input):
         # send the character to the device
         self.ser.write(input + '\r\n')
+
 
     def receive(self):
         # let's wait one second before reading output (let's give device time to answer)
@@ -30,11 +37,13 @@ class lora:
         # return the reply
         return out
 
+
     def send_n_verify(self, input, output):
         self.send(input)
         out = self.receive()
         if out != output + '\r\n':
             print "Failed: " + out
+
 
     def start_up(self):
         self.send_n_verify("mac set appkey FC50E986B86514E4F03F1F2842C5C3D0","ok")
@@ -42,64 +51,71 @@ class lora:
         self.send_n_verify("mac set adr off","ok")
         self.send_n_verify("mac save","ok")
 
+
     def connect(self):
+        # Attemp to join gateway
         self.send_n_verify("mac join otaa","ok")
 
         while True:
-            status = self.receive()
+            status = self.receive().rstrip()
             if status != '':
-                if status == 'accepted\r\n':
-                    print "Join Server Status: " + status.rstrip()
-                    break
-                else:
-                    print "Join Server Status: " + status.rstrip()
-                    self.send_n_verify("mac join otaa","ok")
+                print "Join Server Status: " + status
+                break
+
 
     def build_package(self,heartStatus,heartRate):
+        # Get the current time
         curTime = time.gmtime()
-
+        # Extract the hour and get the binary array
         hour = curTime[3]
         hourB = [int(x) for x in '{:05b}'.format(hour)]
         hourB = hourB[:5]
-
+        # Extract the minute and get the binary array
         minute = curTime[4]
         minuteB = [int(x) for x in '{:06b}'.format(minute)]
         minuteB = minuteB[:6]
-
+        # Extract the second
         second = curTime[5]
-
+        # Get the binary array for the heart status
         heartStatusB = [int(x) for x in '{:05b}'.format(heartStatus)]
         heartStatusB = heartStatusB[:5]
-
+        # Get the binary array for the heart rate
         heartRateB = [int(x) for x in '{:08b}'.format(heartRate)]
         heartRateB = heartRateB[:8]
-
+        # Get the binary array for the message ID number
         msgIDB = [int(x) for x in '{:08b}'.format(self.msgID)]
+        # Calculate the message ID number for next time
         if(self.msgID >= 255):
             self.msgID = 0
         else:
             self.msgID += 1
-
+        # Combine into one array
         messageB = msgIDB + hourB + minuteB + heartStatusB + heartRateB
+        # Convert to number from array of bits
         messageB = BitArray(messageB)
+        # Convert to hex and pad with zeros
         message = format(messageB.uint, 'x').zfill(8)
-
+        # Return the 32-bit hex string
         return message
+
 
     def send_data(self, heartStatus, heartRate):
         # Build the message
         message = self.build_package(heartStatus,heartRate)
-        # Connect to the server
-        self.connect()
         # Send package
         print "Sending data: " + message
         self.send_n_verify("mac tx cnf 1 " + message,"ok")
         while True:
-            status = self.receive()
+            status = self.receive().rstrip()
             if status != '':
-                if status == 'mac_tx_ok\r\n':
-                    print "message status: " + status.rstrip()
+                print "message status: " + status
+                if status == 'mac_tx_ok':
                     break
+                else if status == 'not_joined':
+                    # Try to connect to the gateway
+                    self.connect()
+                    # Then resend the message
+                    self.send_n_verify("mac tx cnf 1 " + message,"ok")
                 else:
-                    print "message status: " + status.rstrip()
+                    # Retry to send the message
                     self.send_n_verify("mac tx cnf 1 " + message,"ok")
