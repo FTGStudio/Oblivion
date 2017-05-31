45b6ea6a9e5baea1067177719fd6ab27c3792bd9
pahlni <pahlni@mail.gvsu.edu>
2017-05-31 20:17:58 +0000

    Read from cyton and send heartrate over LoRa

--- ecg_cyton.py
+++ ecg_cyton.py
@@ -24,7 +24,7 @@ class cyton:
         print 'reset cyton'
 
         # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(2)
+        time.sleep(4)
 
         # Read in the reply
         out = ''
@@ -49,20 +49,20 @@ class cyton:
         self.winCh2 = np.append(self.winCh2, struct.unpack('<i', out[5:8] + '\0')[0])
         self.winCh3 = np.append(self.winCh3, struct.unpack('<i', out[8:11] + '\0')[0])
 
-
     def get_signal(self):
         temp = self.winCh1
         #leave last 2 seconds in window
-        self.winCh1 = self.winCh1[-750:]
-        self.winCh2 = self.winCh2[-750:]
-        self.winCh3 = self.winCh3[-750:]
+        self.winCh1 = self.winCh1[-500:]
+        self.winCh2 = self.winCh2[-500:]
+        self.winCh3 = self.winCh3[-500:]
 
         return temp
 
     def is_window_full(self):
         return len(self.winCh1) >= 1250;
 
-#c = cyton()
-#c.start_stream()
-#while True:
-#    c.read_line()
+if __name__ == "__main__":
+    c = cyton()
+    c.start_stream()
+    while True:
+        c.read_line()
--- ecg_processing.py
+++ ecg_processing.py
@@ -4,20 +4,19 @@ from biosppy import storage
 from heart import heart
 from RepeatedTimer import RepeatedTimer
 from ecg_lora import lora
-<<<<<<< HEAD
 from ecg_cyton import cyton
-=======
 from coms import get_com
 import time
 
 def send_packet_over_lora():
-    mote.send_data(5,a.get_hr_int())
->>>>>>> master
+    temp = int(a.get_avg_heart_rate())
+    print "Sending: "
+    print temp
+    mote.send_data(5,temp)
 
 class ex_proc:
 
     def __init__(self):
-<<<<<<< HEAD
         self.heartRate = []
 
     def add_heart_rate(self, heart_rate):
@@ -34,39 +33,6 @@ class ex_proc:
         print "LORA SEND: " + data
 
 # Set up LoRa mote
-#mote = lora()
-#print "Setting up LoRa mote..."
-#mote.start_up()
-#print "Connecting to LoRa gateway..."
-#mote.connect()
-
-=======
-        self.i=0
-        self.hr = 4
-
-    def get_hr_int(self):
-        return int(self.hr)
-
-    def getData(self,h_obj,signal):
-        if ((self.i*250)+1249 >= len(signal)):
-            self.i = 0
-            print "Reset CSV"
-        # Set the signal to be the previous 5 seconds of data
-        signal_seg = signal[(self.i*250):(self.i*250)+1249]
-        # Set the signal to be processed
-        h_obj.set_signal(signal_seg)
-        # Process signal and show the result in the GUI
-        h_obj.process()
-        # Print the calculated heart rates
-        # h_obj.print_heart_rate()
-        # Print the calculated average heart rate
-        self.hr = h_obj.calc_avg_heart_rate()
-        # print int(self.hr)
-        # Set the time to increase by 3 seconds
-        self.i += 3
-        zzz = int(self.hr)
-
-# Set up LoRa mote
 # Get LoRa COM port
 print "Get LoRa COM Port...",
 lora_com = get_com()
@@ -76,10 +42,6 @@ mote = lora(lora_com)
 # Join server
 mote.connect()
 
-# Import the data from a csv file
-print "Importing data from CSV..."
-signal, mdata = storage.load_txt('nicks_heart_1.csv')
->>>>>>> master
 # Create class for heart process
 h = heart(250)
 
@@ -92,9 +54,7 @@ c = cyton()
 # Set the mote to send every minute
 time.sleep(0.5)
 print "Set timer to send data repeatedly..."
-<<<<<<< HEAD
-#mote_timer = RepeatedTimer(60,mote.send_data,a.get_avg_heart_rate())
-mote_timer = RepeatedTimer(60,a.test_print,a.get_avg_heart_rate())
+mote_timer = RepeatedTimer(15, send_packet_over_lora)
 
 c.start_stream()
 while True:
@@ -112,8 +72,3 @@ while True:
         print "Avg Heartrate"
         print temp
         a.add_heart_rate(temp)
-=======
-# mote_timer = RepeatedTimer(15,mote.send_data,0,61)
-# mote_timer = RepeatedTimer(15,mote.send_data,5,a.get_hr_int)
-mote_timer = RepeatedTimer(60, send_packet_over_lora)
->>>>>>> master
